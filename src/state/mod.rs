47be940ef741b50c15e7d5a248a89a51163abf50
asjn3e <asjn3e@gmail.com>
2024-09-26 16:39:10 +0400

    add state config

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -12,11 +12,14 @@ use std::iter::once;
 use std::marker::PhantomData;
 use std::str::FromStr;
 
-// thread_local! {
-//     pub static STATE: RefCell<Cell<ConfigState, StableMemory>> = RefCell::new(Cell::init(
-//    state_memory(), ConfigState::default())
-//     .expect("failed to initialize stable cell for state"));
-// }
+use crate::ledger_suite_manager::Task;
+use crate::storage::memory::{state_memory, StableMemory};
+
+thread_local! {
+    pub static STATE: RefCell<Cell<ConfigState, StableMemory>> = RefCell::new(Cell::init(
+   state_memory(), ConfigState::default())
+    .expect("failed to initialize stable cell for state"));
+}
 
 const WASM_HASH_LENGTH: usize = 32;
 
@@ -405,4 +408,264 @@ impl ManagedCanisterStatus {
             } => Some(installed_wasm_hash),
         }
     }
+}
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+pub struct LedgerSuiteVersion {
+    pub ledger_compressed_wasm_hash: WasmHash,
+    pub index_compressed_wasm_hash: WasmHash,
+    pub archive_compressed_wasm_hash: WasmHash,
+}
+
+/// Configuration state of the ledger orchestrator.
+#[derive(Clone, PartialEq, Debug, Default)]
+enum ConfigState {
+    #[default]
+    Uninitialized,
+    // This state is only used between wasm module initialization and init().
+    Initialized(State),
+}
+
+impl ConfigState {
+    fn expect_initialized(&self) -> &State {
+        match &self {
+            ConfigState::Uninitialized => trap("BUG: state not initialized"),
+            ConfigState::Initialized(s) => s,
+        }
+    }
+}
+
+impl Storable for ConfigState {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        match &self {
+            ConfigState::Uninitialized => Cow::Borrowed(&[]),
+            ConfigState::Initialized(config) => Cow::Owned(encode(config)),
+        }
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        if bytes.is_empty() {
+            return ConfigState::Uninitialized;
+        }
+        ConfigState::Initialized(decode(bytes.as_ref()))
+    }
+
+    const BOUND: Bound = Bound::Unbounded;
+}
+
+fn encode<S: ?Sized + serde::Serialize>(state: &S) -> Vec<u8> {
+    let mut buf = vec![];
+    ciborium::ser::into_writer(state, &mut buf).expect("failed to encode state");
+    buf
+}
+
+fn decode<T: serde::de::DeserializeOwned>(bytes: &[u8]) -> T {
+    ciborium::de::from_reader(bytes)
+        .unwrap_or_else(|e| panic!("failed to decode state bytes {}: {e}", hex::encode(bytes)))
+}
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+
+pub struct MinimumLedgerSuiteCreationFee(u128);
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+pub struct LedgerSuiteCreationFeeToken {
+    pub icp: MinimumLedgerSuiteCreationFee,
+    pub appic: MinimumLedgerSuiteCreationFee,
+}
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+pub struct State {
+    managed_canisters: ManagedCanisters,
+    // cycles_management: CyclesManagement,
+    more_controller_ids: Vec<Principal>,
+    minter_id: Option<Principal>,
+    /// Locks preventing concurrent execution timer tasks
+    pub active_tasks: BTreeSet<Task>,
+    #[serde(default)]
+    ledger_suite_version: Option<LedgerSuiteVersion>,
+
+    // Collected icp or appic token in the beginning for ledger suite creation
+    collected_icp: u128,
+    collected_appic: u128,
+    minimum_token_for_new_ledger_suite: LedgerSuiteCreationFeeToken,
+}
+
+impl State {
+    pub fn more_controller_ids(&self) -> &[Principal] {
+        &self.more_controller_ids
+    }
+
+    pub fn minter_id(&self) -> Option<&Principal> {
+        self.minter_id.as_ref()
+    }
+
+    // pub fn cycles_management(&self) -> &CyclesManagement {
+    //     &self.cycles_management
+    // }
+
+    // pub fn cycles_management_mut(&mut self) -> &mut CyclesManagement {
+    //     &mut self.cycles_management
+    // }
+
+    pub fn all_managed_canisters_iter(&self) -> impl Iterator<Item = (Erc20Token, &Canisters)> {
+        self.managed_canisters.all_canisters_iter()
+    }
+
+    pub fn all_managed_principals(&self) -> impl Iterator<Item = &Principal> {
+        self.all_managed_canisters_iter()
+            .flat_map(|(_, canisters)| canisters.principals_iter())
+    }
+
+    pub fn all_managed_tokens_ids_iter(&self) -> impl Iterator<Item = Erc20Token> + '_ {
+        self.all_managed_canisters_iter().map(|(id, _)| id)
+    }
+
+    pub fn managed_canisters(&self, token_id: &Erc20Token) -> Option<&Canisters> {
+        self.managed_canisters.find_by_id(token_id)
+    }
+
+    pub fn ledger_suite_version(&self) -> Option<&LedgerSuiteVersion> {
+        self.ledger_suite_version.as_ref()
+    }
+
+    /// Initializes the ledger suite version if it is not already set.
+    /// No-op if the ledger suite version is already set.
+    pub fn init_ledger_suite_version(&mut self, version: LedgerSuiteVersion) {
+        if self.ledger_suite_version.is_none() {
+            self.ledger_suite_version = Some(version);
+        }
+    }
+
+    pub fn update_ledger_suite_version(&mut self, new_version: LedgerSuiteVersion) {
+        self.ledger_suite_version = Some(new_version);
+    }
+
+    fn managed_canisters_mut(&mut self, token_id: &Erc20Token) -> Option<&mut Canisters> {
+        self.managed_canisters.get_mut(token_id)
+    }
+
+    // pub fn managed_status<'a, T: 'a>(
+    //     &'a self,
+    //     token_id: &Erc20Token,
+    // ) -> Option<&'a ManagedCanisterStatus>
+    // where
+    //     Canisters: ManageSingleCanister<T>,
+    // {
+    //     self.managed_canisters(token_id)
+    //         .and_then(|c| c.get().map(|c| &c.status))
+    // }
+
+    // /// Record other canisters managed by the orchestrator.
+    // pub fn record_manage_other_canisters(&mut self, other_canisters: InstalledLedgerSuite) {
+    //     let token_id = TokenId::from(other_canisters.token_symbol.clone());
+    //     self.managed_canisters
+    //         .insert_once(token_id, Canisters::from(other_canisters));
+    // }
+
+    // pub fn record_new_erc20_token(&mut self, contract: Erc20Token, metadata: CanistersMetadata) {
+    //     let token_id = TokenId::from(contract);
+    //     self.managed_canisters
+    //         .insert_once(token_id, Canisters::new(metadata));
+    // }
+
+    // pub fn record_archives(&mut self, token_id: &TokenId, archives: Vec<Principal>) {
+    //     let canisters = self
+    //         .managed_canisters_mut(token_id)
+    //         .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", token_id));
+    //     canisters.archives = archives;
+    // }
+
+    // pub fn record_created_canister<T: Debug>(
+    //     &mut self,
+    //     contract: &Erc20Token,
+    //     canister_id: Principal,
+    // ) where
+    //     Canisters: ManageSingleCanister<T>,
+    // {
+    //     let token_id = TokenId::from(contract.clone());
+    //     let canisters = self
+    //         .managed_canisters_mut(&token_id)
+    //         .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", token_id));
+    //     canisters
+    //         .try_insert(Canister::<T>::new(ManagedCanisterStatus::Created {
+    //             canister_id,
+    //         }))
+    //         .unwrap_or_else(|e| {
+    //             panic!(
+    //                 "BUG: canister {} already created: {:?}",
+    //                 Canisters::display_name(),
+    //                 e
+    //             )
+    //         });
+    // }
+
+    // pub fn record_installed_canister<T>(&mut self, contract: &Erc20Token, wasm_hash: WasmHash)
+    // where
+    //     Canisters: ManageSingleCanister<T>,
+    // {
+    //     let token_id = TokenId::from(contract.clone());
+    //     let managed_canister = self
+    //         .managed_canisters_mut(&token_id)
+    //         .and_then(Canisters::get_mut)
+    //         .unwrap_or_else(|| {
+    //             panic!(
+    //                 "BUG: no managed canisters or no {} canister for {:?}",
+    //                 Canisters::display_name(),
+    //                 token_id
+    //             )
+    //         });
+    //     let canister_id = *managed_canister.canister_id();
+    //     managed_canister.status = ManagedCanisterStatus::Installed {
+    //         canister_id,
+    //         installed_wasm_hash: wasm_hash,
+    //     };
+    // }
+
+    // pub fn validate_config(&self) -> Result<(), InvalidStateError> {
+    //     const MAX_ADDITIONAL_CONTROLLERS: usize = 9;
+    //     if self.more_controller_ids.len() > MAX_ADDITIONAL_CONTROLLERS {
+    //         return Err(InvalidStateError::TooManyAdditionalControllers {
+    //             max: MAX_ADDITIONAL_CONTROLLERS,
+    //             actual: self.more_controller_ids.len(),
+    //         });
+    //     }
+    //     Ok(())
+    // }
+}
+
+pub fn read_state<R>(f: impl FnOnce(&State) -> R) -> R {
+    STATE.with(|cell| f(cell.borrow().get().expect_initialized()))
+}
+
+/// Mutates (part of) the current state using `f`.
+///
+/// Panics if there is no state.
+pub fn mutate_state<F, R>(f: F) -> R
+where
+    F: FnOnce(&mut State) -> R,
+{
+    STATE.with(|cell| {
+        let mut borrowed = cell.borrow_mut();
+        let mut state = borrowed.get().expect_initialized().clone();
+        let result = f(&mut state);
+        borrowed
+            .set(ConfigState::Initialized(state))
+            .expect("failed to write state in stable cell");
+        result
+    })
+}
+
+pub fn init_state(state: State) {
+    STATE.with(|cell| {
+        let mut borrowed = cell.borrow_mut();
+        assert_eq!(
+            borrowed.get(),
+            &ConfigState::Uninitialized,
+            "BUG: State is already initialized and has value {:?}",
+            borrowed.get()
+        );
+        borrowed
+            .set(ConfigState::Initialized(state))
+            .expect("failed to initialize state in stable cell")
+    });
 }
